a76a3aa7790f84584bfa56772d9e12240f01f753
Sergey Chernov <serega.morph@gmail.com>
2023-03-20 10:37:00 +0100

    Configuration

--- spring/src/main/java/com/seregamorph/restapi/partial/JacksonBootConfig.java
+++ spring/src/main/java/com/seregamorph/restapi/partial/JacksonBootConfig.java
@@ -1,7 +1,9 @@
 package com.seregamorph.restapi.partial;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class JacksonBootConfig {
 
     @Bean
