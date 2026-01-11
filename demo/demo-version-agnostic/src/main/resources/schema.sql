f20215f3f2ab8b18ce4168491aa730ebb2060738
seregamorph <serega.morph@gmail.com>
2026-01-11 12:47:47 +0100

    H2 compatibility

--- demo/demo-version-agnostic/src/main/resources/schema.sql
+++ demo/demo-version-agnostic/src/main/resources/schema.sql
@@ -1,25 +1,23 @@
 CREATE TABLE `team` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `manager_id` int(11) DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `manager_id` int DEFAULT NULL,
+  `name` varchar(255) NOT NULL UNIQUE,
   `description` varchar(255) DEFAULT NULL,
   `created_date` datetime NOT NULL,
   `last_modified_date` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `team_name` (`name`)
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `person` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `team_id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `team_id` int NOT NULL,
   `name` varchar(255),
   `year_of_birth` varchar(255),
-  `email_address` varchar(255),
+  `email_address` varchar(255) UNIQUE,
   `activation_date` varchar(255),
   `created_date` datetime NOT NULL,
   `last_modified_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `person_email_address` (`email_address`),
   CONSTRAINT `person_team_fk` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`)
 );
 
@@ -28,7 +26,7 @@ ADD CONSTRAINT `team_person_fk`
 FOREIGN KEY (`manager_id`) REFERENCES `person` (`id`);
 
 CREATE TABLE `checkin` (
-  `person_id` int(11) NOT NULL,
+  `person_id` int NOT NULL,
   `checkin_date` date NOT NULL,
   `message` MEDIUMTEXT,
   `created_date` datetime NOT NULL,
